4a2c8b28a90c78247be89379ee9de34b6eaac496
Jindrg <jindrg@gmail.com>
2023-05-27 14:58:25 -0300

    Creacion de Arquitectura del Proyecto

---
+++ app/src/main/java/ar/edu/ort/tp3/appmobile_kotlin_carsapi/Login.kt
@@ -0,0 +1,11 @@
+package ar.edu.ort.tp3.appmobile_kotlin_carsapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
